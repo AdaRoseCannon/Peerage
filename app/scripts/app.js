ab314dbf7652c00c81be8e030f0fa1102e93f7ac
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-09-27 20:41:41 +0100

    Working on sending files.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,10 +17,23 @@ function addMessage (name, data) {
 	messages.appendChild(node);
 }
 
+function processData(data) {
+	'use strict';
+	switch(data.type) {
+	case 'message':
+		addMessage (data.user, data.data);
+		break;
+	case 'file':
+		addMessage (data.user + ' is sending', data.file);
+		break;
+	}
+}
+
 function recieveData (data) {
 	'use strict';
-	addMessage (data.user, data.data);
-	if (timestamps.indexOf[data.timestamp] === -1) {
+	useDataConn.updateListDisplay();
+	if (timestamps.indexOf(data.timestamp) === -1) {
+		processData(data);
 		//This message is new to me so I will retransmit to make sure everyone else has it.
 		timestamps.push(data.timestamp);
 		useDataConn.retransmit(data);
@@ -31,13 +44,22 @@ function UseDataConn () {
 	'use strict';
 	this.connections = {};
 
-	this.add = function (dataConn) {
-		this.connections[dataConn.peer] = dataConn;
+	this.updateListDisplay = function () {
 		var d = '';
 		for(var i in this.connections) {
-			d = d + this.connections[i].peer + ', ';
+			if (this.connections[i].open) {
+				d = d + this.connections[i].peer + ', ';
+			} else {
+				this.connections[i].close();
+				delete this.connections[i];
+			}
 		}
 		theirId.get(0).value = d;
+	};
+
+	this.add = function (dataConn) {
+		this.connections[dataConn.peer] = dataConn;
+		this.updateListDisplay();
 		dataConn.on('data',function (data) {
 			recieveData (data);
 		});
@@ -48,20 +70,22 @@ function UseDataConn () {
 
 	this.send = function (data) {
 		var timestamp = Date.now();
+		timestamps.push(timestamp);
 		for(var i in this.connections) {
-			this.connections[i].send({timestamp: timestamp, user: peerId, data: data});
+			this.connections[i].send({timestamp: timestamp, user: peerId, type:'message', data: data});
 		}
 	};
 
-	this.send = function (data) {
+	this.sendFile = function (file) {
 		var timestamp = Date.now();
 		timestamps.push(timestamp);
 		for(var i in this.connections) {
-			this.connections[i].send({timestamp: timestamp, user: peerId, data: data});
+			this.connections[i].send({timestamp: timestamp, user: peerId, type:'file', file: file.name, filetype: file.type});
 		}
 	};
 
 	this.retransmit = function (data) {
+		console.log('Retransmitting ' + JSON.stringify(data));
 		for(var i in this.connections) {
 			this.connections[i].send(data);
 		}
@@ -76,17 +100,22 @@ function UseDataConn () {
 
 useDataConn = new UseDataConn();
 
-define([], function () {
-    'use strict';
-	theirId.get(0).value = '';
-    var peer = new Peer({host:'/', port: '9000', debug:3});
+define(['dropzone-amd-module'], function (Dropzone) {
+	'use strict';
 
-    peer.on('open', function(id) {
+	var myDropzone = new Dropzone('#dropzone', { url: '/'});
+
+	console.log(myDropzone);
+
+	theirId.get(0).value = '';
+	var peer = new Peer({host:'/', port: '9000', debug:3});
+
+	peer.on('open', function(id) {
 		myId.get(0).value = id;
 		peerId = id;
 	});
 
-    connectBtn.on('click', function () {
+	connectBtn.on('click', function () {
 		var dataConn = peer.connect(theirId.get(0).value, {
 			reliable: false
 		});
@@ -94,27 +123,33 @@ define([], function () {
 			useDataConn.add(dataConn);
 			addMessage ('Connected to', dataConn.peer);
 		});
-    });
+	});
 
-    peer.on('connection', function (dataConn) {
-		useDataConn.add(dataConn);
-		addMessage ('User has connected', dataConn.peer);
-    });
+	peer.on('connection', function (dataConn) {
+		dataConn.on('open', function() {
+			useDataConn.add(dataConn);
+			addMessage ('User has connected', dataConn.peer);
+		});
+	});
 
-    peer.on('error', function (error) {
+	peer.on('error', function (error) {
 		addMessage ('Error', error);
-    });
+	});
 
-    messageBtn.on('click', function () {
+	messageBtn.on('click', function () {
 		var data = textData.get(0).value;
 		useDataConn.send(data);
 		addMessage('me', data);
-    });
+	});
+	myDropzone.on('addedfile', function (file) {
+		useDataConn.sendFile(file);
+		addMessage('making file available: ', file.name);
+	});
 
 	window.onunload=function(){
 		useDataConn.send('Disconnecting');
 		useDataConn.close();
 	};
 
-    return '\'Allo \'Allo!';
+	return '\'Allo \'Allo!';
 });
