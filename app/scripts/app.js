f4bf0f4ea7feee2c5f5ad85aad8b674739b767c6
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-09-27 14:33:44 +0100

    Making progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,13 @@
-/*global define */
+/*global define, Peer*/
+var myId = document.getElementById('myId');
+var theirId = document.getElementById('theirId');
 define([], function () {
     'use strict';
+    var peer = new Peer({host:'/', port: '9000', debug:3});
+
+    peer.on('open', function(id) {
+		myId.value = id;
+	});
 
     return '\'Allo \'Allo!';
 });
